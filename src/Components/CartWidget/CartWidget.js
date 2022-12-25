680035746df0353e3097bf95d7f588b715f4b645
rodrihern <rohernandez003@gmail.com>
2022-12-25 13:17:48 -0300

    navbar actualizada

--- src/Components/CartWidget/CartWidget.js
+++ src/Components/CartWidget/CartWidget.js
@@ -4,9 +4,19 @@ import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 export const CartWidget = ({ cartNum = 0 }) => {
 
+    if (cartNum > 9) {
+        const numerito = document.querySelector('.cart-number')
+        numerito.classList.add("little-num")
+    } 
+
+    if (cartNum > 99) {
+        const numerito = document.querySelector('.cart-number')
+        numerito.classList.add("plus99")
+    }
+
     return (
         <a className='cart hidden-content' href="#">
-            <div className='cart-number'>{ cartNum > 9 ? "+9" : cartNum }</div>
+            <div className='cart-number'>{ cartNum > 99 ? "+99" : cartNum }</div>
             <FontAwesomeIcon className='fa-lg' icon={ faCartShopping } />
         </a>
     );
