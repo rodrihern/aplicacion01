e57420a7925771da68182cf247e058eea5bdc026
rodrihern <rohernandez003@gmail.com>
2023-01-12 15:44:47 -0300

    preEntrega2 Hernandez

--- src/Components/Error404/Error404.js
+++ src/Components/Error404/Error404.js
@@ -7,12 +7,14 @@ export const Error404 = () => {
 
     return (
         
-        <div class="mainbox">
-            <div class="err">4</div>
-            <i class="far fa-question-circle fa-spin"></i>
-            <FontAwesomeIcon className="far fa-spin" icon={faQuestionCircle} />
-            <div class="err2">4</div>
-            <div class="msg">Parece que esta pagina ya no existe, fue movida, o jamas existió.
+        <div className="mainbox">
+            <div className="err404">
+                <div className="err">4</div>
+                <FontAwesomeIcon className="far fa-spin" icon={faQuestionCircle} />
+                <div className="err2">4</div>
+            </div>
+            
+            <div className="msg">Parece que esta pagina ya no existe, fue movida, o jamas existió.
                 <p className="msg-p">Vayamos al <Link className="err-link" to="/productos">inicio</Link> y probemos desde ahí.</p>
             </div>
         </div>
