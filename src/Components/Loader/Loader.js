f143806688626ef9f0a1f3127488e499f81f963c
rodrihern <rohernandez003@gmail.com>
2023-01-21 01:18:19 -0300

    loader agregado mejor

--- src/Components/Loader/Loader.js
+++ src/Components/Loader/Loader.js
@@ -1,6 +1,5 @@
 import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import companyLogo from '../NavBar/Fan store icon.png'
 import React from 'react'
 import './Loader.css'
 
@@ -8,7 +7,6 @@ function Loader() {
     return (
         <div className='loader'>
             <FontAwesomeIcon className='fa-spin fa-regular cargando' icon={ faSpinner } />
-            <img className='loading-logo' height={60} src={ companyLogo } alt="logo" />
         </div>
     )
 }
